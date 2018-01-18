14569e813dbce664a829223892f9c0f04da08f4f
Carlos Vallejo (UST <C60193@ust-global.com>
2018-01-18 12:44:30 -0800

    update copyright message

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/process/RequestTransformer.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/process/RequestTransformer.java
@@ -1,5 +1,5 @@
 // =========================================================================
-// Copyright 2017 T-Mobile USA, Inc.
+// Copyright 2018 T-Mobile, US
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// See the readme.txt file for additional language around disclaimer of warranties.
 // =========================================================================
 
 package com.tmobile.cso.vault.api.process;
