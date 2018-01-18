701c1330d9d93281a37cd3d60cb8359353a951ea
CarlosVall <carlos.vallejo92@yahoo.com>
2018-01-18 12:26:44 -0800

    Update install_tvault.sh

--- install_tvault.sh
+++ install_tvault.sh
@@ -2,7 +2,7 @@
 #set -x
 
 # =========================================================================
-# Copyright 2017 T-Mobile USA, Inc.
+# Copyright 2018 T-Mobile, US
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# See the readme.txt file for additional language around disclaimer of warranties.
 # =========================================================================
 
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
